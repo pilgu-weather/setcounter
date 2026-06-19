de68e5513a87fb5ca60021f06f934dde218e1146
kang pilgu <dfwsf@hanmail.net>
2026-06-20 08:28:01 +0900

    Rename app to Korean title

--- static/js/app.js
+++ static/js/app.js
@@ -139,11 +139,11 @@ async function showWorkoutReminder() {
   if (registration?.active) {
     registration.active.postMessage({
       type: "SHOW_WORKOUT_REMINDER",
-      title: "SET COUNTER",
+      title: "흐엇?!",
       body: "오전 11시입니다. 오늘 운동 기록하러 갑시다.",
     });
   } else if (registration?.showNotification) {
-    registration.showNotification("SET COUNTER", {
+    registration.showNotification("흐엇?!", {
       body: "오전 11시입니다. 오늘 운동 기록하러 갑시다.",
       icon: "/static/assets/urus1.png?v=2",
       tag: "daily-workout-reminder",
